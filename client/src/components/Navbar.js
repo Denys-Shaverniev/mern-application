bef39ff0262ffd0a4ba7217b3bb53a4340e1967f
Denis Shavernev <denis.shavernev14@gmail.com>
2021-09-30 14:41:01 +0300

    remove client gitlink

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React, { useContext } from "react"
+import { NavLink, useHistory } from "react-router-dom"
+import { AuthContext } from "../context/AuthContext"
+
+
+export const Navbar = () => {
+    const history = useHistory()
+    const auth = useContext(AuthContext)
+
+    const logoutHandler = (e) => {
+        e.preventDefault()
+        auth.logout()
+        history.push('/')
+    }
+
+    return (
+        <nav >
+            <div className="nav-wrapper blue lighten-2" style={{ padding: '0 2em' }}>
+                <span className="brand-logo">{'</>'}</span>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li><NavLink to="/create">Создать</NavLink></li>
+                    <li><NavLink to="/links">Ссылки</NavLink></li>
+                    <li><a href="/"
+                        className="btn white black-text"
+                        onClick={logoutHandler}
+                    >
+                        Выход
+                    </a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
